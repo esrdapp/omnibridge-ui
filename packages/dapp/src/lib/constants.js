e31c20defb2c97815bfebcc1e85b3cb7b9074ae1
ESR <82104698+esrdapp@users.noreply.github.com>
2022-02-05 02:35:48 +0000

    Update constants.js

--- packages/dapp/src/lib/constants.js
+++ packages/dapp/src/lib/constants.js
@@ -1,18 +1,17 @@
 import { BigNumber } from 'ethers';
 
-export const ETH_XDAI_BRIDGE = 'eth-xdai';
-export const BSC_XDAI_BRIDGE = 'bsc-xdai';
-export const POA_XDAI_BRIDGE = 'poa-xdai';
-export const KOVAN_SOKOL_BRIDGE = 'kovan-sokol';
-export const ETH_BSC_BRIDGE = 'eth-bsc';
+export const HPB_ETH_BRIDGE = 'hpb-eth';
+export const HPB_BSC_BRIDGE = 'hpb-bnb';
+export const HPB_GNO_BRIDGE = 'hpb-xdai';
+
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000';
 export const ETHER_CURRENCY_LOGO =
   'https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png';
 export const BNB_CURRENCY_LOGO =
   'https://s2.coinmarketcap.com/static/img/coins/64x64/1839.png';
-export const POA_CURRENCY_LOGO =
-  'https://s2.coinmarketcap.com/static/img/coins/64x64/2548.png';
+export const XDAI_CURRENCY_LOGO =
+  'https://s2.coinmarketcap.com/static/img/coins/64x64/8635.png';
 
 export const LARGEST_UINT256 = BigNumber.from(
   '115792089237316195423570985008687907853269984665640564039457584007913129639935',
@@ -27,9 +26,9 @@ export const DEFAULT_BRIDGE_DIRECTION =
 export const COINZILLA_API_KEY =
   process.env.REACT_APP_COINZILLA_API_KEY || null;
 
-export const NON_ETH_CHAIN_IDS = [56, 77, 99, 100];
+export const NON_ETH_CHAIN_IDS = [56, 100, 269];
 
-export const XDAI_CHAIN_IDS = [77, 99, 100];
+export const HPB_CHAIN_IDS = [269];
 
 export const nativeCurrencies = {
   1: {
@@ -43,17 +42,7 @@ export const nativeCurrencies = {
     homeTokenAddress:
       '0x6A023CCd1ff6F2045C3309768eAd9E68F978f6e1'.toLowerCase(),
   },
-  42: {
-    chainId: 42,
-    decimals: 18,
-    logoURI: ETHER_CURRENCY_LOGO,
-    address: ADDRESS_ZERO,
-    name: 'Kovan Ether',
-    symbol: 'KETH',
-    mode: 'NATIVE',
-    homeTokenAddress:
-      '0x3D14493DF2B479E6BABE82Fc2373F91622bac025'.toLowerCase(),
-  },
+
   56: {
     chainId: 56,
     decimals: 18,
@@ -65,45 +54,54 @@ export const nativeCurrencies = {
     homeTokenAddress:
       '0xCa8d20f3e0144a72C6B5d576e9Bd3Fd8557E2B04'.toLowerCase(),
   },
-  99: {
-    chainId: 99,
+  
+  100: {
+    chainId: 100,
     decimals: 18,
-    logoURI: POA_CURRENCY_LOGO,
-    name: 'POA',
+    logoURI: XDAI_CURRENCY_LOGO,
+    name: 'XDAI',
     address: ADDRESS_ZERO,
-    symbol: 'POA',
+    symbol: 'XDAI',
     mode: 'NATIVE',
     homeTokenAddress:
       '0x9fe3864F9Ae7cfb5668Dae90C0e20c4C3D437664'.toLowerCase(),
   },
+  
+    269: {
+    chainId: 269,
+    decimals: 18,
+    logoURI: HPB_CURRENCY_LOGO,
+    name: 'HPB',
+    address: ADDRESS_ZERO,
+    symbol: 'HPB',
+    mode: 'NATIVE',
+    homeTokenAddress:
+      '0x9fe3864F9Ae7cfb5668Dae90C0e20c4C3D437664'.toLowerCase(),
+  },
+  
+  
+  
 };
 
 export const nativeCurrencyMediators = {
-  1: '0xa6439ca0fcba1d0f80df0be6a17220fed9c9038a'.toLowerCase(),
-  42: '0x227a6f13aa0dba8912d740c0f88fb1304b2597e1'.toLowerCase(),
-  56: '0xefc33f8b2c4d51005585962be7ea20518ea9fd0d'.toLowerCase(),
-  99: '0xF6a1Ad94d29679388e533B63bfE1Fd6f1680D23B'.toLowerCase(),
+  1: '0x000000000'.toLowerCase(),
+  56: '0x000000000'.toLowerCase(),
+  100: '0x000000000'.toLowerCase(),
+  269: '0x000000000'.toLowerCase(),
 };
 
 export const networkNames = {
-  1: 'ETH Mainnet',
-  42: 'Kovan Testnet',
+  1: 'Ethereum',
   56: 'Binance Smart Chain',
-  77: 'Sokol Testnet',
-  99: 'POA Network',
   100: 'Gnosis Chain',
+  269: 'HPB',
 };
 
 export const networkLabels = {
-  1: 'Mainnet',
-  3: 'Ropsten',
-  4: 'Rinkeby',
-  5: 'Görli',
-  42: 'Kovan',
+  1: 'Ethereum',
   56: 'BSC',
-  77: 'Sokol',
-  99: 'POA',
   100: 'Gnosis Chain',
+  269: 'HPB',
 };
 
 export const networkCurrencies = {
@@ -111,49 +109,33 @@ export const networkCurrencies = {
     name: 'Ethereum',
     symbol: 'ETH',
   },
-  42: {
-    name: 'Ethereum',
-    symbol: 'ETH',
-  },
   56: {
     name: 'Binance Coin',
     symbol: 'BNB',
-  },
-  77: {
-    name: 'Sokol POA',
-    symbol: 'SPOA',
-  },
-  99: {
-    name: 'POA',
-    symbol: 'POA',
   },
   100: {
     name: 'xDai',
     symbol: 'xDai',
   },
+  269: {
+    name: 'HPB',
+    symbol: 'HPB',
+  },
 };
 
 const {
-  REACT_APP_MAINNET_RPC_URL,
-  REACT_APP_XDAI_RPC_URL,
-  REACT_APP_POA_RPC_URL,
-  REACT_APP_SOKOL_RPC_URL,
-  REACT_APP_KOVAN_RPC_URL,
+  REACT_APP_ETHEREUM_RPC_URL,
   REACT_APP_BSC_RPC_URL,
+  REACT_APP_GNOSIS_RPC_URL,
+  REACT_APP_HPB_RPC_URL,
 } = process.env;
 
 export const chainUrls = {
   1: {
-    rpc: REACT_APP_MAINNET_RPC_URL.split(' '),
+    rpc: REACT_APP_ETHEREUM_RPC_URL.split(' '),
     explorer: 'https://blockscout.com/eth/mainnet',
     chainId: 1,
     name: networkNames[1],
-  },
-  42: {
-    rpc: REACT_APP_KOVAN_RPC_URL.split(' '),
-    explorer: 'https://blockscout.com/eth/kovan',
-    chainId: 42,
-    name: networkNames[42],
   },
   56: {
     rpc: REACT_APP_BSC_RPC_URL.split(' '),
@@ -161,46 +143,35 @@ export const chainUrls = {
     chainId: 56,
     name: networkNames[56],
   },
-  77: {
-    rpc: REACT_APP_SOKOL_RPC_URL.split(' '),
-    explorer: 'https://blockscout.com/poa/sokol',
-    chainId: 77,
-    name: networkNames[77],
-  },
-  99: {
-    rpc: REACT_APP_POA_RPC_URL.split(' '),
-    explorer: 'https://blockscout.com/poa/core',
-    chainId: 99,
-    name: networkNames[99],
-  },
   100: {
-    rpc: REACT_APP_XDAI_RPC_URL.split(' '),
+    rpc: REACT_APP_GNOSIS_RPC_URL.split(' '),
     explorer: 'https://blockscout.com/xdai/mainnet',
     chainId: 100,
     name: networkNames[100],
   },
+  269: {
+    rpc: REACT_APP_HPB_RPC_URL.split(' '),
+    explorer: 'https://hscan.org',
+    chainId: 269,
+    name: networkNames[269],
+  },
 };
 
 export const defaultTokensUrl = {
-  100: 'https://tokens.honeyswap.org',
   1: 'https://tokens.uniswap.org',
-  42: '',
-  77: '',
-  99: '',
   56: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/smartchain/tokenlist.json',
+  100: 'https://tokens.honeyswap.org',
+  269: 'https://dex.hpbnode.com/download/hpdex.tokenlist.json',
 };
 
-export const GRAPH_HEALTH_ENDPOINT =
-  'https://api.thegraph.com/index-node/graphql';
+export const GRAPH_HEALTH_ENDPOINT = 'http://localhost:8030/graphql';
 
 export const LOCAL_STORAGE_KEYS = {
   DONT_SHOW_CLAIMS: 'dont-show-claims',
-  MAINNET_RPC_URL: 'mainnet-rpc-url',
-  XDAI_RPC_URL: 'xdai-rpc-url',
-  POA_RPC_URL: 'poa-rpc-url',
+  ETHEREUM_RPC_URL: 'ethereum-rpc-url',
   BSC_RPC_URL: 'bsc-rpc-url',
-  KOVAN_RPC_URL: 'kovan-rpc-url',
-  SOKOL_RPC_URL: 'sokol-rpc-url',
+  GNOSIS_RPC_URL: 'gnosis-rpc-url',
+  HPB_RPC_URL: 'hpb-rpc-url',
   NEVER_SHOW_CLAIMS: 'never-show-claims',
   INFINITE_UNLOCK: 'infinite-unlock',
   CUSTOM_TOKENS: 'customTokens',
